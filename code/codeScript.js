a92e8be47900e98b6675ed1c5ed16f87bc284f15
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-05-12 15:53:05 +0200

    updated background art; improved responsive design

--- code/codeScript.js
+++ code/codeScript.js
@@ -17,37 +17,37 @@ var projectsJSON = [
         "name": "ChromNewTab",
         "date": "2020",
         "text": "I was sick and tired of my NewTab page and didn't find any good ones on the WebStore. It didn't really work... it does work as a Chrome Addon, but Chrome has some really strict policies which exceed my skills. So the project is on hold right now.",
-        "link": "https://manuelfahmy.de/"
+        "link": "https://github.com/cr4yfish/chrome-new-tab"
     },
     {    
         "name": "First React.js App",
         "date": "2021",
         "text": "I thought I was ready for stepping up my game to a WebApp, but I was wrong. At the time, I had no idea what I was doing. Be ready for the return of it soon(tm)!",
-        "link": "https://manuelfahmy.de/"
+        "link": ""
     },
     {    
         "name": "AnimeScraper",
         "date": "2021",
         "text": "Since my failure of understanding React.js, I had a new plan: <ul> <li>Learn vanilla JS</li> <li>Learn React.js</li> <li>??</li ><li>Profit!</li> </ul>Jokes aside, for this project I used more Javascript than all of the Javascript of my previous projects combined. It features different kinds of loops, sorting algorithms, AJAX, Javascript HTML Rendering and lots more! I think it actually generates more HTML from Javascript than there is non-generated-HTML.",
-        "link": "https://manuelfahmy.de/"
+        "link": "https://github.com/cr4yfish/animeScraper"
     },
     {    
         "name": "Localhost Dashboard",
         "date": "2021",
         "text": "Big story: I had multiple problems with CORS and figured it would be easier to host the website properly. So I got XAMPP and, using Javascript, coded up a Website that scrapes all my local projects on my PC and lists them in a neat UI. It uses AJAX to get names and directory paths from my projects. It also generate hrefs, so I can open any (local) website within XAMPP.",
-        "link": "https://manuelfahmy.de/"
+        "link": ""
     },
     {    
         "name": "Wallet-Website",
         "date": "2021",
         "text": "I was looking to invest some money into a crypto Project, together with a couple of friends, and didn't like the Website for it. So I tried creating my own UI that scrapes content from the original Website and uses it for individial Profit that's linked to Accounts in my working Account system. It features a working regristration process, login process and encrypted password storage in a MySQL database. I also had to learn some more PHP for this.",
-        "link": "https://manuelfahmy.de/"
+        "link": "https://github.com/cr4yfish/wallet-website"
     },
     {    
         "name": "isTodayTuesday",
         "date": "2021",
         "text": "Amid my finals in School, I was bored and wanted to code up a small project in an hour. This is the result. It tells you, using simple javascript, if it's tuesday. The Javascript system is built ready for expanding it (it checks the day from a variable, so it would be possible to upgrade it to a system where you can check if it's each day).",
-        "link": "https://manuelfahmy.de/"
+        "link": "https://github.com/cr4yfish/isTodayTuesday"
     },
     {    
         "name": "Portfolio Website 2.0",
@@ -69,6 +69,7 @@ for (i = 0; i < projectsJSON.length; i++) {
 
     let entryTitle = document.createElement("span");
     entryTitle.setAttribute("class", "entryTitle rubik_regular pointer");
+    entryTitle.setAttribute("onclick", "window.location.href='" + projectsJSON[i].link + "';");
 
     let entryDate = document.createElement("span");
     entryDate.setAttribute("class", "entryDate rubik_light");
