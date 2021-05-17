ebfec362ae20f612b646027d5c50306b2eef5894
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-05-17 17:29:50 +0200

    moved caterpillar Stuff into a new folder to resolve git issues

---
+++ projects/caterpillarCalculator/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Caterpillar calculator</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="fonts.css">
+
+        <link rel="stylesheet" href="https://use.typekit.net/rts3cue.css">
+
+    </head>
+
+    <body onload="cleaner();addListener();">
+
+    <?php 
+    
+    $servername = "rdbms.strato.de";
+    $user = "dbu596396";
+    $pw = "FpDAtg7neWIL9E";
+    
+    $con = new mysqli($servername, $user, $pw);
+
+    if($con->connect_error) {
+        die("SQL Connection failed!");
+    }
+    echo "SQL Connected";
+
+    ?>
+
+
+        <div id="contentWrapper">
+
+
+            <div id="calcWrapper">
+
+                <div class="listEntry">
+                    <span class="biloLight listEntryName smallHeader">Person</span>
+                    <span class="biloLight listEntryUEC smallHeader">aUEC</span>
+                </div>
+
+                <div class="listEntry">
+                    <span class="biloLight listEntryName">Anil</span>
+                        <input id="input1" onkeyup="addDot(this);" type="text" placeholder="aUEC amount"  class="inputCheck biloLight listEntryUEC listEntryInput">
+                </div>
+
+                <div class="spacer"></div>
+
+                <div class="listEntry">
+                    <span class="biloLight listEntryName">Hannes</span>
+                        <input id="input2" onkeyup="addDot(this);" placeholder="aUEC amount"  class="inputCheck biloLight listEntryUEC listEntryInput" type="text">
+                </div>
+
+                <div class="spacer"></div>
+
+                <div class="listEntry">
+                    <span class="biloLight listEntryName">Manuel</span>
+                        <input id="input3" onkeyup="addDot(this);" placeholder="aUEC amount" class="inputCheck biloLight listEntryUEC listEntryInput" type="text">
+                </div>
+
+                <div class="listEntry">
+                    <span class="biloLight listEntryName">Gesamt</span>
+                        <span id="together" class="biloLight listEntryUEC listEntryInput">0</span>
+                </div>
+            </div>
+
+            <button class="biloLight btn btn-primary" onclick="nextExec();">next</button>
+
+        </div>
+        
+        <script src="mainScript.js" async defer></script>
+    </body>
+</html>
---
+++ projects/caterpillarCalculator/mainScript.js
@@ -0,0 +1,106 @@
+var catPrice = 4686000;
+
+function cleaner() {
+
+    localStorage.removeItem("isBuyable");
+
+    var valuesHTMLcollection = document.getElementsByClassName("listEntryInput");
+    for(i = 0; i < Array.from(valuesHTMLcollection).length; i++) {
+        valuesHTMLcollection[i].value = null;
+    }
+    document.getElementById("together").textContent = "0";
+}
+
+function drawElements() {
+    var contentWrapperElement = document.getElementById("contentWrapper");
+
+    var resultWrapperElement = document.createElement("div");
+    resultWrapperElement.setAttribute("id", "resultWrapper");
+
+    var textHeader = document.createElement("span");
+    textHeader.textContent = "Is it enough for a Caterpillar?";
+    textHeader.setAttribute("class", "smallHeader isEnough");
+
+    var resultPrintElement = document.createElement("div");
+    resultPrintElement.setAttribute("id", "resultPrint");
+
+    var restAmountElement = document.createElement("span");
+    restAmountElement.setAttribute("id", "restAmount");
+    restAmountElement.setAttribute("class", "smallHeader");
+
+    var btnElement = document.createElement("button");
+    btnElement.setAttribute("class", "btn btn-primary");
+    btnElement.setAttribute("onclick", "back();")
+    btnElement.textContent = "back";
+
+    resultWrapperElement.appendChild(textHeader);
+    resultWrapperElement.appendChild(resultPrintElement);
+    resultWrapperElement.appendChild(restAmountElement);
+    resultWrapperElement.appendChild(btnElement);
+
+    contentWrapperElement.prepend(resultWrapperElement);
+}
+
+function nextExec() {
+
+    var valuesHTMLcollection = document.getElementsByClassName("inputCheck");
+    console.log(valuesHTMLcollection);
+    var playerAmount = 0;
+
+    for(i = 0; i < Array.from(valuesHTMLcollection).length; i++) {
+        console.log(valuesHTMLcollection[i]);
+        let tempAmount = parseInt(valuesHTMLcollection[i].value.split(".").join("").match(/\d/g).join(''), 10);
+
+        playerAmount = playerAmount + tempAmount;
+    }
+    
+    if(playerAmount >= catPrice) {
+        drawElements();
+        document.getElementById("resultPrint").textContent = "Yes";
+
+    } else {
+        drawElements();
+        document.getElementById("resultPrint").textContent = "No";
+        document.getElementById("restAmount").textContent = "Rest amount needed: " + (catPrice - playerAmount) + " aUEC";
+
+    }
+
+}
+
+function back() {
+    cleaner();
+    document.getElementById("resultWrapper").remove();
+}
+
+function addListener() {
+    var valuesHTMLcollection = document.getElementsByClassName("listEntryInput");
+
+    for(i = 0; i < Array.from(valuesHTMLcollection).length; i++) {
+
+        var togetherElement = document.getElementById("together")
+        valuesHTMLcollection[i].addEventListener("keyup", function() {
+            console.log("Keyup event!");
+            togetherElement.textContent =  parseInt(document.getElementById("input1").value.match(/\d/g).join(''), 10) + parseInt(document.getElementById("input2").value.match(/\d/g).join(''), 10) + parseInt(document.getElementById("input3").value.match(/\d/g).join(''), 10);
+            
+            let ele = togetherElement;
+
+            ele = ele.textContent.split('.').join('');    // Remove dash (-) if mistakenly entered.
+        
+            let finalVal = ele.match(/.{1,3}(?=(.{3})*$)/g).join('.');
+            togetherElement.textContent = finalVal;
+
+        })
+    }
+
+}
+
+function addDot(element) {
+
+    let ele = document.getElementById(element.id);
+
+    ele = ele.value.split('.').join('');    // Remove dash (-) if mistakenly entered.
+
+    let finalVal = ele.match(/.{1,3}(?=(.{3})*$)/g).join('.');
+    document.getElementById(element.id).value = finalVal;
+
+}
