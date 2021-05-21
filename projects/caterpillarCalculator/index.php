a41e502479dca78e41314bf49dbb2bff795f1929
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-05-21 15:27:20 +0200

    updated caterpillar calc

--- projects/caterpillarCalculator/index.php
+++ projects/caterpillarCalculator/index.php
@@ -18,18 +18,7 @@
 
     <?php 
     
-    $servername = "rdbms.strato.de";
-    $user = "dbu596396";
-    $pw = "FpDAtg7neWIL9E";
-    $dbname = "dbs2000829";
-    
-    $con = mysqli_connect($servername, $user, $pw, $dbname);
-
-    if($con->connect_error) {
-        die("SQL Connection failed!");
-    }
-
-    $result = mysqli_query($con, "SELECT * FROM moneten");
+        include_once "connection.php";
 
     ?>
 
