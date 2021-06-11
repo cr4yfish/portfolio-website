616d9d10b2cb756bdff3c4889ccd2ae3bd8af2c0
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-06-11 16:04:34 +0200

    added navbarItems open animations

--- script.js
+++ script.js
@@ -32,10 +32,39 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-function openNav() {
+function resetAnimation() {
+  localStorage.removeItem("navbarHasBeenOpened");
+}
+
+async function openNav() {
+
+  if (localStorage.getItem("navbarHasBeenOpened") != "true") {
+      console.log(document.querySelectorAll("#sidebar a"));
+
+      // set all items off screen
+      var navbarItems = document.querySelectorAll("#sidebar a");
+      for(i = 1; i < navbarItems.length; i++) {
+
+        navbarItems[i].style.transform = "translateX(200%)";
+        navbarItems[i].style.transition = "transform 0.5s"
+      }
+  }
+
+
   document.getElementById("sidebar").style.width = "250px";
   //document.getElementById("mainContent").style.marginRight = "250px";
   document.getElementById("navbarOpener").style.opacity = "0";
+
+  // fade the items back in one by one
+  await sleep(50);
+  for(i = 1; i < navbarItems.length; i++) {
+
+    navbarItems[i].style.transform = "translateX(0)";
+
+    await sleep(50);
+  }
+
+  localStorage.setItem("navbarHasBeenOpened", "true");
   
 }
 
